3ab3f765419025e161ad77343a89bd1ea569a5fa
priyam7214 <priyamrathodrathodpriyam@gmail.com>
2025-05-02 12:16:29 +0530

    Add files via upload

---
+++ 4unary_operators.c
@@ -0,0 +1,17 @@
+/* concept of unary operators*/
+#include<stdio.h>
+void main()
+{
+    int a=10,b;
+    b= ++a; //b=11 a=11
+    printf("b = %d a = %d",b,a);
+
+    b= a++; // b=11 a=12
+    printf("\nb = %d a = %d",b,a);
+
+    b= --a; // b=10 a=10
+    printf("\nb = %d a = %d",a,b);
+
+    b= a--; // b=10 a=9
+    printf("\nb = %d a = %d",a,b);
+}
